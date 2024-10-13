a604d0469620123353782bd356e16902c26dad15
PiyushKhurana <khuranapiyush27@gmail.com>
2024-10-13 10:34:08 +0530

    feat: read graph from txt

--- src/BH.java
+++ src/BH.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -6,14 +8,16 @@ public class BH {
 
     public static void main(String[] args) throws IOException {
         // Example graph represented as an adjacency matrix
-        int[][] graph = {
-                {0, 7, 9, 0, 0, 14},
-                {7, 0, 10, 15, 0, 0},
-                {9, 10, 0, 11, 0, 2},
-                {0, 15, 11, 0, 6, 0},
-                {0, 0, 0, 6, 0, 9},
-                {14, 0, 2, 0, 9, 0}
-        };
+//        int[][] graph = {
+//                {0, 7, 9, 0, 0, 14},
+//                {7, 0, 10, 15, 0, 0},
+//                {9, 10, 0, 11, 0, 2},
+//                {0, 15, 11, 0, 6, 0},
+//                {0, 0, 0, 6, 0, 9},
+//                {14, 0, 2, 0, 9, 0}
+//        };
+
+        int[][] graph = readFromFile(10000);
 
         int source = 0;
 
@@ -44,5 +48,39 @@ public class BH {
                 }
             }
         }
+    }
+
+    private static int[][] readFromFile(int vertices) throws IOException {
+        String savedGameFile = "/Users/piyush/"+"SavedGraphs/Graph_"+vertices+".txt";
+
+        BufferedReader reader;
+        reader = new BufferedReader(new FileReader(savedGameFile));
+
+        int lines = 0;
+        while (reader.readLine() != null) lines++;
+        reader.close();
+
+        reader = new BufferedReader(new FileReader(savedGameFile));
+
+        int[][] graph = new int[lines][lines];
+
+
+        String line = "";
+        int row = 0;
+        while((line = reader.readLine()) != null)
+        {
+            String[] cols = line.split(","); //note that if you have used space as separator you have to split on " "
+            int col = 0;
+            for(String  c : cols)
+            {
+                graph[row][col] = Integer.parseInt(c);
+                col++;
+            }
+            row++;
+        }
+        reader.close();
+
+        //  printGraph(graph);
+        return graph;
     }
 }
